c8dd3c08166ac4b3f5f1a45d0943751766f58105
Ruben Menino <ruben.menino@outlook.com>
2021-05-20 23:36:50 +0100

    initial

---
+++ bombeiros/src/test/java/com/example/bombeiros/BombeirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bombeiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombeirosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

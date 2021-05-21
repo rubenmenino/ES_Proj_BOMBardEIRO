9462057da943ff7b54b7e14fe110968b732f1749
Ruben Menino <ruben.menino@outlook.com>
2021-05-21 17:12:06 +0100

    jenkins

---
+++ src/test/java/com/example/es_proj_bombardeiro/EsProjBomBardEiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es_proj_bombardeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsProjBomBardEiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
